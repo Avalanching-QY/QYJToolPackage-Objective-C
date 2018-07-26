9968f695b8c61070fe69c133bceed37a50163ccd
Avalanching <18269285634@163.com>
2018-07-26 11:45:53 +0800

    Create Build

---
+++ QYJToolPackage-Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYJToolPackage-Objective-C
+//
+//  Created by Avalanching on 2018/7/26.
+//  Copyright © 2018年 Avalanching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYJToolPackage-Objective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYJToolPackage-Objective-C
+//
+//  Created by Avalanching on 2018/7/26.
+//  Copyright © 2018年 Avalanching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
